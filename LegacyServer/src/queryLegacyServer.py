923e7ed570c6b2ebcdfebd3c0a937f2aba8ed18e
Glen McManus <glen.don.mcmanus@gmail.com>
2022-02-27 01:42:51 -0800

    add env files as placeholder for each image; adjust compose; all containers run

--- LegacyServer/src/queryLegacyServer.py
+++ LegacyServer/src/queryLegacyServer.py
@@ -9,24 +9,18 @@ from subprocess import Popen, PIPE, run
 #reformat ouput in some way
 #send output to transaction server (possibly a different file)
 
+
 async def query_server(stock_symbol, username):
     proc = Popen(['nc 192.168.4.2 4444'], stdin=PIPE, stdout=PIPE, shell=True)
     proc.stdin.write(("" + stock_symbol + " " + username + "\r").encode("utf-8"))
     output = await proc.communicate()[0]
     return output.decode("utf-8")
 
+
 def process_request(stock_symbol, username):
     query_string = query_server(stock_symbol, username)
     return query_string.split(",")
 
-def main():
-    stock_info = process_request("TES", "fakeUser")
-    print(stock_info)
-    print(stock_info[0])
-    print(stock_info[1])
-    print(stock_info[2])
-    print(stock_info[3])
-    print(stock_info[4])
 
 async def handle_request(reader, writer):
     data = await reader.read(100)
@@ -50,7 +44,8 @@ async def handle_request(reader, writer):
     print("Close the connection")
     writer.close()
 
-def service_loop():
+
+async def service_loop():
     if os.environ.__contains__("FETCH_IP"):
         print(os.environ["FETCH_IP"])
         my_ip = os.environ["FETCH_IP"]
@@ -61,7 +56,7 @@ def service_loop():
         print(os.environ["FETCH_PORT"])
         my_port = os.environ["FETCH_PORT"]
     else:
-        my_port = 8889
+        my_port = 8888
 
     server = await asyncio.start_server(
         handle_request, my_ip, my_port)
@@ -72,7 +67,20 @@ def service_loop():
     async with server:
         await server.serve_forever()
 
+
 asyncio.run(service_loop())
 
-if __name__ == "__main__":
-    main()
+
+#def main():
+#    stock_info = process_request("TES", "fakeUser")
+#    print(stock_info)
+#    print(stock_info[0])
+#    print(stock_info[1])
+#    print(stock_info[2])
+#    print(stock_info[3])
+#    print(stock_info[4])
+
+
+#if __name__ == "__main__":
+#    main()
+
