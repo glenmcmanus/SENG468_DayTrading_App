6d7e1b08d09c0edc38e0229d46d821c0a3af607a
Glen McManus <glen.don.mcmanus@gmail.com>
2022-03-15 01:53:27 -0700

    uncomment dispatch line input

--- WorkloadDispatcher/WorkloadDispatcher.py
+++ WorkloadDispatcher/WorkloadDispatcher.py
@@ -57,7 +57,7 @@ def main():
             time.sleep(int(line[1]))
             continue
 
-        #print("line after split: ", line)
+        print("line after split: ", line)
         req_str = 'http://localhost:9000/' + line[0].lower()
 
         if len(line) > 1:
