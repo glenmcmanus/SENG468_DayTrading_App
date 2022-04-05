3532924600f4cb3c4de50ebcaf1b13dea593c0d4
Glen McManus <gmcmanus@uvic.ca>
2022-04-05 05:08:51 -0700

    fix multi workload

--- WorkloadDispatcher/MultiDispatcher.sh
+++ WorkloadDispatcher/MultiDispatcher.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
 my_pids=()
+counter=0
 i=0
 n=5
 
+echo m
+
 for f in $1/*
 do
-  echo $f
+  echo $counter
 
-  if [ ${#my_pids[@]} -eq $n ]
+  if [[ ${#my_pids[@]} -eq n ]]
   then
-    wait ${my_pids[$i]}
+    wait ${my_pids[$i]} 
     python3 WorkloadDispatcher.py $f &
-    my_pids[$i]=(&!)
+    pid=&!
+    my_pids[$i]=${pid}
   else
     python3 WorkloadDispatcher.py $f &
-    my_pids+=(&!)
+    pid=&!
+    my_pids[$i]=${pid}
   fi
 
+  counter=$((counter+1))
   i=$((i+1))
   i=$((i%n))
 
